e21e7dd5026384bc35675d8761ce2c02cb8dcb99
Younguk Kim <bluekyu.dev@gmail.com>
2018-11-11 23:05:25 +0900

    [recording] Add GUI

---
+++ recording/tools/gui/src/main.cpp
@@ -0,0 +1,65 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Younguk Kim (bluekyu)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <memory>
+
+#include <boost/dll/alias.hpp>
+
+#include <rpplugins/rpstat/gui_interface.hpp>
+
+#include <rpplugins/recording/plugin.hpp>
+
+namespace rpplugins {
+
+class PluginGUI : public GUIInterface
+{
+public:
+    PluginGUI(rpcore::RenderPipeline& pipeline);
+    virtual ~PluginGUI() = default;
+
+    void on_imgui_new_frame() override;
+
+private:
+    RecordingPlugin* plugin_;
+};
+
+// ************************************************************************************************
+
+PluginGUI::PluginGUI(rpcore::RenderPipeline& pipeline): GUIInterface(pipeline, RPPLUGINS_GUI_ID_STRING)
+{
+    plugin_ = static_cast<decltype(plugin_)>(pipeline_.get_plugin_mgr()->get_instance(RPPLUGINS_GUI_ID_STRING)->downcast());
+}
+
+void PluginGUI::on_imgui_new_frame()
+{
+    ImGui::Begin("Recording Plugin Window");
+
+    ImGui::Text("Test");
+
+    ImGui::End();
+}
+
+}
+
+RPPLUGINS_GUI_CREATOR(rpplugins::PluginGUI)
